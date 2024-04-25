1097b108053c6f4ace0cdc29082aa9b6a9405d46
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-25 16:37:03 +0900

    add: 회원가입 시 발생하는 에러코드 추가

--- src/main/java/com/thinktank/global/error/model/ErrorCode.java
+++ src/main/java/com/thinktank/global/error/model/ErrorCode.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 public enum ErrorCode {
 	// 400
 	BAD_REQUEST("[❎ ERROR]"),
+	FAIL_WRONG_PASSWORD("[❎ ERROR] 비밀번호가 일치하지 않습니다."),
 
 	// 401
 	FAIL_UNAUTHORIZED_EXCEPTION("[❎ ERROR] 로그인이 필요한 기능입니다."),
@@ -23,7 +24,11 @@ public enum ErrorCode {
 	// 404
 	FAIL_NOT_POST_FOUND_EXCEPTION("[❎ ERROR] 요청하신 게시물을 찾을 수 없습니다."),
 	FAIL_NOT_USER_FOUND_EXCEPTION("[❎ ERROR] 요청하신 회원을 찾을 수 없습니다."),
-	FAIL_NOT_COMMENT_FOUND_EXCEPTION("[❎ ERROR] 요청하신 댓글을 찾을 수 없습니다.");
+	FAIL_NOT_COMMENT_FOUND_EXCEPTION("[❎ ERROR] 요청하신 댓글을 찾을 수 없습니다."),
+
+	// 409
+	FAIL_EMAIL_CONFLICT("[❎ ERROR] 이미 존재하는 이메일입니다."),
+	FAIL_NICKNAME_CONFLICT("[❎ ERROR] 이미 존재하는 닉네임입니다.");
 
 	private String message;
 }
