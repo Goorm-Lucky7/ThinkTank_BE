2562084319e531feac9b7d3ec97c71ed28094ef3
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-26 01:05:27 +0900

    feat: 토큰 관련 예외 코드 추가

--- src/main/java/com/thinktank/global/error/model/ErrorCode.java
+++ src/main/java/com/thinktank/global/error/model/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
 
 	// 401
 	FAIL_UNAUTHORIZED_EXCEPTION("[❎ ERROR] 로그인이 필요한 기능입니다."),
+	FAIL_TOKEN_EXPIRE_EXCEPTION("[❎ ERROR] 토큰이 만료되었습니다."),
 
 	//403
 	POST_POST_FORBIDDEN_EXCEPTION("[❎ ERROR] 게시글 작성 권한이 없습니다."),
@@ -25,6 +26,7 @@ public enum ErrorCode {
 	FAIL_NOT_POST_FOUND_EXCEPTION("[❎ ERROR] 요청하신 게시물을 찾을 수 없습니다."),
 	FAIL_NOT_USER_FOUND_EXCEPTION("[❎ ERROR] 요청하신 회원을 찾을 수 없습니다."),
 	FAIL_NOT_COMMENT_FOUND_EXCEPTION("[❎ ERROR] 요청하신 댓글을 찾을 수 없습니다."),
+	FAIL_NOT_TOKEN_FOUND_EXCEPTION("[❎ ERROR] 토큰이 존재하지 않습니다."),
 
 	// 409
 	FAIL_EMAIL_CONFLICT("[❎ ERROR] 이미 존재하는 이메일입니다."),
