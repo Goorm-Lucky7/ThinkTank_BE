4c4d9ad93774dd44386ecfd33f2acb456507cc73
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-25 21:56:14 +0900

    feat: 로그인 성공 시 다중 토큰 발급 로직 구현

--- src/main/java/com/thinktank/global/auth/filter/JWTLoginFilter.java
+++ src/main/java/com/thinktank/global/auth/filter/JWTLoginFilter.java
@@ -2,6 +2,7 @@ package com.thinktank.global.auth.filter;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,10 +10,10 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.thinktank.global.auth.jwt.JWTTokenProvider;
-import com.thinktank.global.auth.service.ClientDetails;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -22,11 +23,15 @@ public class JWTLoginFilter extends UsernamePasswordAuthenticationFilter {
 
 	private static final int HTTP_STATUS_UNAUTHORIZED = 401;
 
-	private static final long TOKEN_EXPIRATION_TIME_MS = 60 * 60 * 10L;
+	private static final long ACCESS_TOKEN_EXPIRATION_TIME_MS = 60 * 60 * 10L;
 
-	private static final String AUTHORIZATION_HEADER = "Authorization";
+	private static final long REFRESH_TOKEN_EXPIRATION_TIME_MS = 60 * 60 * 24 * 1000L;
 
-	private static final String TOKEN_PREFIX = "Bearer ";
+	private static final int COOKIE_MAX_AGE_ONE_DAY = 24 * 60 * 60;
+
+	private static final String ACCESS_TOKEN_HEADER = "access";
+
+	private static final String REFRESH_TOKEN_COOKIE_NAME = "refresh";
 
 	private final AuthenticationManager authenticationManager;
 
@@ -50,12 +55,23 @@ public class JWTLoginFilter extends UsernamePasswordAuthenticationFilter {
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 		Authentication authResult) throws IOException, ServletException {
 
-		ClientDetails clientDetails = (ClientDetails)authResult.getPrincipal();
+		String username = authResult.getName();
 
-		String username = clientDetails.getUsername();
-		String token = jwtTokenProvider.createJwt(username, TOKEN_EXPIRATION_TIME_MS);
+		String accessToken = jwtTokenProvider.createJwt(
+			ACCESS_TOKEN_HEADER,
+			username,
+			ACCESS_TOKEN_EXPIRATION_TIME_MS
+		);
 
-		response.addHeader(AUTHORIZATION_HEADER, TOKEN_PREFIX + token);
+		String refreshToken = jwtTokenProvider.createJwt(
+			REFRESH_TOKEN_COOKIE_NAME,
+			username,
+			REFRESH_TOKEN_EXPIRATION_TIME_MS
+		);
+
+		response.setHeader(ACCESS_TOKEN_HEADER, accessToken);
+		response.addCookie(createCookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken));
+		response.setStatus(HttpStatus.OK.value());
 	}
 
 	@Override
@@ -63,5 +79,14 @@ public class JWTLoginFilter extends UsernamePasswordAuthenticationFilter {
 		AuthenticationException failed) throws IOException, ServletException {
 
 		response.setStatus(HTTP_STATUS_UNAUTHORIZED);
+	}
+
+	private Cookie createCookie(String key, String value) {
+
+		Cookie cookie = new Cookie(key, value);
+		cookie.setMaxAge(24 * 60 * 60);
+		cookie.setHttpOnly(true);
+
+		return cookie;
 	}
 }
