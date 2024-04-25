1590427dcc84298a1a06d5c94aac84372ae51306
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-25 14:42:04 +0900

    feat: 임시로 모든 HTTP 요청에 대한 접근 허용 및 비밀번호 인코딩 방식 설정

---
+++ src/main/java/com/thinktank/global/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.thinktank.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+		httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+
+		httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+
+		httpSecurity.authorizeHttpRequests((auth) -> auth
+			.anyRequest().permitAll()
+		);
+
+		httpSecurity.sessionManagement((session) -> session
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		);
+
+		return httpSecurity.build();
+	}
+}
