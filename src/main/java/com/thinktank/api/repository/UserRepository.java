11fcb2458ac17e6dedc59547cbcd1f456d424315
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-25 15:16:26 +0900

    feat: 주어진 닉네임이 이미 존재하는지 확인하는 기능 구현

--- src/main/java/com/thinktank/api/repository/UserRepository.java
+++ src/main/java/com/thinktank/api/repository/UserRepository.java
@@ -7,4 +7,6 @@ import com.thinktank.api.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	Boolean existsByEmail(String email);
+
+	Boolean existsByNickname(String nickname);
 }
