32c7d5e347a03b81ae4f54b0f3bf0f8b0d91fded
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-28 03:33:27 +0900

    feat: 리프레시 토큰 정보 저장 및 사용자 인증 정보 반환 로직 구현

--- src/main/java/com/thinktank/api/service/auth/AuthorizationService.java
+++ src/main/java/com/thinktank/api/service/auth/AuthorizationService.java
@@ -1,12 +1,19 @@
 package com.thinktank.api.service.auth;
 
+import static com.thinktank.global.common.util.AuthConstants.*;
+
 import java.util.Date;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.thinktank.api.entity.TokenSave;
+import com.thinktank.api.entity.User;
 import com.thinktank.api.repository.auth.TokenRepository;
+import com.thinktank.global.auth.service.ClientDetails;
 import com.thinktank.global.common.util.CookieUtils;
+import com.thinktank.global.config.TokenConfig;
 import com.thinktank.global.error.exception.UnauthorizedException;
 import com.thinktank.global.error.model.ErrorCode;
 
@@ -18,17 +25,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthorizationService {
 
-	private static final String REFRESH_TOKEN_COOKIE_NAME = "refresh";
-
-	private static final String ACCESS_TOKEN_TYPE = "access";
-
-	private static final long ACCESS_TOKEN_EXPIRY_DURATION = 600000L;
-
-	private static final long REFRESH_TOKEN_EXPIRY_DURATION = 86400000L;
-
 	private final JwtProviderService jwtProviderService;
 
+	private final JwtAuthenticationService jwtAuthenticationService;
+
 	private final TokenRepository tokenRepository;
+
+	private final TokenConfig tokenConfig;
+
+	public void addRefreshToken(String username, String refreshToken, long expirationTimeMs) {
+
+		Date date = new Date(System.currentTimeMillis() + expirationTimeMs);
+
+		TokenSave tokenSave = TokenSave.builder()
+			.username(username)
+			.refreshToken(refreshToken)
+			.expiration(date.toString())
+			.build();
+
+		tokenRepository.save(tokenSave);
+	}
 
 	public void refreshAccessToken(HttpServletRequest request, HttpServletResponse response) {
 
@@ -38,50 +54,50 @@ public class AuthorizationService {
 		validateTokenNotExpired(refreshToken);
 		validateTokenIsRefresh(refreshToken);
 
-		String username = jwtProviderService.getUsername(refreshToken);
+		String username = jwtProviderService.getUsernameFromToken(refreshToken);
 
-		String newAccessToken = jwtProviderService.createJwt(
-			ACCESS_TOKEN_TYPE,
+		String newAccessToken = jwtProviderService.provideAccessToken(
+			ACCESS_TOKEN_HEADER,
 			username,
-			ACCESS_TOKEN_EXPIRY_DURATION
+			tokenConfig.getAccessTokenExpirationTimeMs()
 		);
-		String newRefreshToken = jwtProviderService.createJwt(
+		String newRefreshToken = jwtProviderService.provideAccessToken(
 			REFRESH_TOKEN_COOKIE_NAME,
 			username,
-			REFRESH_TOKEN_EXPIRY_DURATION
+			tokenConfig.getRefreshTokenExpirationTimeMs()
 		);
 
 		tokenRepository.deleteByRefreshToken(refreshToken);
-		addRefreshToken(username, refreshToken, REFRESH_TOKEN_EXPIRY_DURATION);
+		addRefreshToken(username, refreshToken, tokenConfig.getRefreshTokenExpirationTimeMs());
 
-		response.setHeader(ACCESS_TOKEN_TYPE, newAccessToken);
+		response.setHeader(ACCESS_TOKEN_HEADER, newAccessToken);
 		response.addCookie(CookieUtils.tokenCookie(REFRESH_TOKEN_COOKIE_NAME, newRefreshToken));
+	}
+
+	public Authentication getAuthentication(String token) {
+
+		String username = jwtProviderService.getUsernameFromToken(token);
+		User user = User.builder().email(username).build();
+		ClientDetails clientDetails = new ClientDetails(user);
+
+		return new UsernamePasswordAuthenticationToken(
+			clientDetails,
+			null,
+			clientDetails.getAuthorities()
+		);
 	}
 
 	private void validateTokenNotExpired(String refreshToken) {
 
-		if (jwtProviderService.isExpired(refreshToken)) {
+		if (jwtAuthenticationService.isTokenExpired(refreshToken)) {
 			throw new UnauthorizedException(ErrorCode.FAIL_TOKEN_EXPIRE_EXCEPTION);
 		}
 	}
 
 	private void validateTokenIsRefresh(String refreshToken) {
 
-		if (!jwtProviderService.getCategory(refreshToken).equals(REFRESH_TOKEN_COOKIE_NAME)) {
+		if (!jwtProviderService.getCategoryFromToken(refreshToken).equals(REFRESH_TOKEN_COOKIE_NAME)) {
 			throw new UnauthorizedException(ErrorCode.FAIL_NOT_TOKEN_FOUND_EXCEPTION);
 		}
-	}
-
-	private void addRefreshToken(String username, String refresh, Long expiredMs) {
-
-		Date date = new Date(System.currentTimeMillis() + expiredMs);
-
-		TokenSave tokenSave = TokenSave.builder()
-			.username(username)
-			.refreshToken(refresh)
-			.expiration(date.toString())
-			.build();
-
-		tokenRepository.save(tokenSave);
 	}
 }
