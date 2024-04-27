7d07ec94b4475efbe4d86852996da2724295547a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-27 22:00:06 +0900

    feat: 공통 상수 정의

---
+++ src/main/java/com/thinktank/global/common/util/AuthConstants.java
@@ -0,0 +1,11 @@
+package com.thinktank.global.common.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class AuthConstants {
+
+	public static final String ACCESS_TOKEN_HEADER = "access";
+
+	public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh";
+}
