2333251c9b2dcfd506c20f7a26d4f222c004553e
ChangMinPark2 <98qkrckdals@naver.com>
2024-04-29 02:39:21 +0900

    feat: 실패 종류 추가

--- src/main/java/com/thinktank/global/error/model/ErrorCode.java
+++ src/main/java/com/thinktank/global/error/model/ErrorCode.java
@@ -13,6 +13,10 @@ public enum ErrorCode {
 	FAIL_WRONG_PASSWORD("[❎ ERROR] 비밀번호가 일치하지 않습니다."),
 	FAIL_INVALID_CATEGORY("[❎ ERROR] 유효하지 않은 카테고리입니다."),
 	FAIL_INVALID_LANGUAGE("[❎ ERROR] 유효하지 않은 카테고리입니다."),
+	BAD_REQUEST_COMPILE_ERROR("[❎실패] 컴파일 에러 입니다."),
+	BAD_REQUEST_RUNTIME_ERROR("[❎실패] 런타임 에러 입니다."),
+	BAD_REQUEST_FAIL("[❎실패] 테스트케이스를 통과하지 못했습니다."),
+	BAD_REQUEST_TIME_OUT("[❎실패] 시간 초과입니다."),
 
 	// 401
 	FAIL_UNAUTHORIZED_EXCEPTION("[❎ ERROR] 로그인이 필요한 기능입니다."),
