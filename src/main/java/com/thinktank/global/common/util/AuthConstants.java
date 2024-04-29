99e2f2eaff1392c61d3154cd9d7c7bc23ea8284b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-29 17:54:54 +0900

    refactor: 인스턴스 방지를 위해 @NoArgsConstructor 사용

--- src/main/java/com/thinktank/global/common/util/AuthConstants.java
+++ src/main/java/com/thinktank/global/common/util/AuthConstants.java
@@ -1,8 +1,9 @@
 package com.thinktank.global.common.util;
 
-import lombok.experimental.UtilityClass;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-@UtilityClass
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class AuthConstants {
 
 	public static final String ACCESS_TOKEN_HEADER = "access";
