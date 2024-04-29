6343e0d4b01f0a76146abd0dbcd962e4e0717a7e
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-29 17:28:50 +0900

    refactor: 토큰 재발급 로직 리팩토링

--- src/main/java/com/thinktank/api/service/auth/JwtProviderService.java
+++ src/main/java/com/thinktank/api/service/auth/JwtProviderService.java
@@ -1,61 +1,138 @@
 package com.thinktank.api.service.auth;
 
+import static com.thinktank.global.common.util.AuthConstants.*;
+
 import java.util.Date;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.thinktank.api.entity.User;
+import com.thinktank.api.entity.auth.AuthUser;
+import com.thinktank.api.repository.UserRepository;
 import com.thinktank.global.config.TokenConfig;
+import com.thinktank.global.error.exception.NotFoundException;
+import com.thinktank.global.error.model.ErrorCode;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JwtProviderService {
 
+	private static final String EMAIL = "email";
+	private static final String NICKNAME = "nickname";
+
 	private final TokenConfig tokenConfig;
+	private final UserRepository userRepository;
 
-	public String getUsernameFromToken(String token) {
+	public String generateAccessToken(String email, String nickname) {
+		final Date issuedDate = new Date();
+		final Date expiredDate = new Date(issuedDate.getTime() + tokenConfig.getAccessTokenExpire());
 
-		return parseClaims(token).get("username", String.class);
-	}
-
-	public String getCategoryFromToken(String token) {
-
-		return parseClaims(token).get("category", String.class);
-	}
-
-	public String provideAccessToken(String category, String username, long accessExpireMs) {
-
-		return commonInfo(category, username, accessExpireMs);
-	}
-
-	public String provideRefreshToken(String category, String username, long refreshExpireMs) {
-
-		return commonInfo(category, username, refreshExpireMs);
-	}
-
-	private String commonInfo(String category, String username, long expireMs) {
-
-		Date issuDate = new Date();
-		Date expireDate = new Date(issuDate.getTime() + expireMs);
-
-		return Jwts.builder()
-			.claim("category", category)
-			.claim("username", username)
-			.issuedAt(issuDate)
-			.expiration(expireDate)
-			.signWith(tokenConfig.getSecretKey())
+		return buildJwt(issuedDate, expiredDate)
+			.claim(EMAIL, email)
+			.claim(NICKNAME, nickname)
 			.compact();
 	}
 
-	private Claims parseClaims(String token) {
+	public String generateRefreshToken(String email) {
+		final Date issuedDate = new Date();
+		final Date expiredDate = new Date(issuedDate.getTime() + tokenConfig.getRefreshTokenExpire());
 
+		return buildJwt(issuedDate, expiredDate)
+			.claim(EMAIL, email)
+			.compact();
+	}
+
+	@Transactional
+	public String reGenerateToken(String refreshToken, HttpServletResponse response) {
+		final Claims claims = getClaimsByToken(refreshToken);
+		final String email = claims.get(EMAIL, String.class);
+		final User user = userRepository.findByEmail(email)
+			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_NOT_USER_FOUND_EXCEPTION));
+
+		validateRefreshToken(refreshToken, user.getRefreshToken());
+
+		final String newAccessToken = generateAccessToken(user.getEmail(), user.getNickname());
+		final String newRefreshToken = generateRefreshToken(user.getEmail());
+
+		user.updateRefreshToken(newRefreshToken);
+		response.setHeader(ACCESS_TOKEN_HEADER, newAccessToken);
+		response.setHeader(REFRESH_TOKEN_HEADER, newRefreshToken);
+
+		return newAccessToken;
+	}
+
+	public String extractToken(String header, HttpServletRequest request) {
+		String token = request.getHeader(header);
+
+		if (token == null || !token.startsWith(BEARER)) {
+			log.warn("{} IS NULL OR NOT BEARER", header);
+			return null;
+		}
+
+		return token.replaceFirst(BEARER, "")
+			.trim();
+	}
+
+	public AuthUser extractAuthUserByAccessToken(String token) {
+		final Claims claims = getClaimsByToken(token);
+		final String email = claims.get(EMAIL, String.class);
+		final String nickname = claims.get(NICKNAME, String.class);
+
+		return AuthUser.create(email, nickname);
+	}
+
+	public boolean isUsable(String token) {
+		try {
+			Jwts.parser()
+				.verifyWith(tokenConfig.getSecretKey())
+				.build()
+				.parseSignedClaims(token);
+
+			return true;
+		} catch (ExpiredJwtException e) {
+			log.warn("TOKEN EXPIRED");
+			throw new NotFoundException(ErrorCode.FAIL_TOKEN_EXPIRE_EXCEPTION);
+		} catch (IllegalArgumentException e) {
+			log.warn("TOKEN IS NULL");
+			throw new NotFoundException(ErrorCode.FAIL_NOT_TOKEN_FOUND_EXCEPTION);
+		} catch (Exception e) {
+			log.warn("INVALID TOKEN");
+			throw new NotFoundException(ErrorCode.FAIL_INVALID_TOKEN);
+		}
+	}
+
+	private JwtBuilder buildJwt(Date issuedDate, Date expiredDate) {
+		return Jwts.builder()
+			.issuedAt(issuedDate)
+			.expiration(expiredDate)
+			.signWith(tokenConfig.getSecretKey(), Jwts.SIG.HS256)
+			.setHeaderParam("alg", "HS256")
+			.setHeaderParam("typ", "JWT");
+	}
+
+	private Claims getClaimsByToken(String token) {
 		return Jwts.parser()
 			.verifyWith(tokenConfig.getSecretKey())
 			.build()
 			.parseSignedClaims(token)
 			.getPayload();
+	}
+
+	private void validateRefreshToken(String currentRefreshToken, String savedRefreshToken) {
+		if (!currentRefreshToken.equals(savedRefreshToken)) {
+			log.warn("INVALID REFRESH TOKEN");
+			throw new NotFoundException(ErrorCode.FAIL_INVALID_TOKEN);
+		}
 	}
 }
