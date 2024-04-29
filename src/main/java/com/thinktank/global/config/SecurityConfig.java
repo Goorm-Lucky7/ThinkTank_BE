e386d60fe46a8c3ada2c952ad9ef2be76e6617d9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-29 17:27:07 +0900

    feat: 로그인 필터 등록

--- src/main/java/com/thinktank/global/config/SecurityConfig.java
+++ src/main/java/com/thinktank/global/config/SecurityConfig.java
@@ -1,54 +1,52 @@
 package com.thinktank.global.config;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
-import com.thinktank.api.repository.auth.TokenRepository;
-import com.thinktank.api.service.auth.AuthorizationService;
-import com.thinktank.api.service.auth.JwtAuthenticationService;
 import com.thinktank.api.service.auth.JwtProviderService;
 import com.thinktank.global.auth.filter.AuthorizationFilter;
-import com.thinktank.global.auth.filter.JwtAuthenticationFilter;
-import com.thinktank.global.auth.filter.JwtLoginFilter;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final AuthenticationConfiguration authenticationConfiguration;
-
 	private final JwtProviderService jwtProviderService;
+	private final HandlerExceptionResolver handlerExceptionResolver;
 
-	private final TokenRepository tokenRepository;
-
-	private final JwtAuthenticationService jwtAuthenticationService;
-
-	private final AuthorizationService authorizationService;
-
-	private final TokenConfig tokenConfig;
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws
-		Exception {
-
-		return authenticationConfiguration.getAuthenticationManager();
+	public SecurityConfig(
+		JwtProviderService jwtProviderService,
+		@Qualifier("handlerExceptionResolver") HandlerExceptionResolver handlerExceptionResolver
+	) {
+		this.jwtProviderService = jwtProviderService;
+		this.handlerExceptionResolver = handlerExceptionResolver;
 	}
 
 	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return webSecurity -> webSecurity.ignoring()
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+			.requestMatchers("/h2-console/**")
+			.requestMatchers("/api/signup")
+			.requestMatchers("/api/login");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
 
 		return new BCryptPasswordEncoder();
 	}
@@ -56,39 +54,24 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-		httpSecurity.csrf(AbstractHttpConfigurer::disable);
-
-		httpSecurity.formLogin(AbstractHttpConfigurer::disable);
-
-		httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+		httpSecurity.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
 
 		httpSecurity.authorizeHttpRequests((auth) -> auth
-			.requestMatchers("/login", "/api/signup").permitAll()
-			.requestMatchers("/api/reissue").permitAll()
+			.requestMatchers("/api/login", "/api/signup").permitAll()
 			.anyRequest().authenticated()
 		);
 
 		httpSecurity.addFilterBefore(
-			new AuthorizationFilter(jwtAuthenticationService, authorizationService),
-			JwtLoginFilter.class
-		);
-
-		httpSecurity.addFilterBefore(
-			new JwtAuthenticationFilter(jwtProviderService, jwtAuthenticationService, tokenRepository),
-			LogoutFilter.class
-		);
-
-		httpSecurity.addFilterAt(
-			new JwtLoginFilter(
-				tokenConfig, authenticationManager(authenticationConfiguration), jwtProviderService,
-				authorizationService
-			),
+			new AuthorizationFilter(jwtProviderService, handlerExceptionResolver),
 			UsernamePasswordAuthenticationFilter.class
 		);
 
-		httpSecurity.sessionManagement((session) -> session
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		);
+		httpSecurity.exceptionHandling((exceptionHandling) -> {
+			HttpStatusEntryPoint httpStatusEntryPoint = new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED);
+			exceptionHandling.authenticationEntryPoint(httpStatusEntryPoint);
+		});
 
 		return httpSecurity.build();
 	}
