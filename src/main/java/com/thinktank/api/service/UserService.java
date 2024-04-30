4dcceedc3a18e5355b0bd1903832e03a7f93faa6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-30 14:50:42 +0900

    feat: 로그아웃 기능 서비스에 추가

--- src/main/java/com/thinktank/api/service/UserService.java
+++ src/main/java/com/thinktank/api/service/UserService.java
@@ -1,5 +1,7 @@
 package com.thinktank.api.service;
 
+import static com.thinktank.global.common.util.AuthConstants.*;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,14 +10,21 @@ import com.thinktank.api.dto.user.request.LoginReqDto;
 import com.thinktank.api.dto.user.request.SignUpDto;
 import com.thinktank.api.dto.user.response.LoginResDto;
 import com.thinktank.api.entity.User;
+import com.thinktank.api.entity.auth.AuthUser;
 import com.thinktank.api.repository.UserRepository;
 import com.thinktank.api.service.auth.JwtProviderService;
+import com.thinktank.global.common.util.CookieUtils;
 import com.thinktank.global.error.exception.BadRequestException;
 import com.thinktank.global.error.exception.NotFoundException;
 import com.thinktank.global.error.model.ErrorCode;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -38,7 +47,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public LoginResDto login(LoginReqDto loginReqDto) {
+	public LoginResDto login(LoginReqDto loginReqDto, HttpServletResponse response) {
 		final User user = userRepository.findByEmail(loginReqDto.email())
 			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_NOT_USER_FOUND_EXCEPTION));
 
@@ -48,7 +57,29 @@ public class UserService {
 		final String refreshToken = jwtProviderService.generateRefreshToken(user.getEmail());
 		user.updateRefreshToken(refreshToken);
 
+		response.setHeader(ACCESS_TOKEN_HEADER, accessToken);
+
+		Cookie refreshTokenCookie = CookieUtils.generateRefreshTokenCookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken);
+		response.addCookie(refreshTokenCookie);
+
 		return new LoginResDto(accessToken, refreshToken);
+	}
+
+	@Transactional
+	public void logout(HttpServletRequest request, HttpServletResponse response) {
+		String refreshToken = jwtProviderService.extractRefreshToken(REFRESH_TOKEN_COOKIE_NAME, request);
+
+		AuthUser authUser = jwtProviderService.extractAuthUserByAccessToken(refreshToken);
+		if (authUser != null) {
+			final User user = userRepository.findByEmail(authUser.email())
+				.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_NOT_USER_FOUND_EXCEPTION));
+
+			user.updateRefreshToken(null);
+		}
+
+		Cookie refreshTokenCookie = CookieUtils.expireRefreshTokenCookie(REFRESH_TOKEN_COOKIE_NAME);
+
+		response.addCookie(refreshTokenCookie);
 	}
 
 	private void validateEmailNotExists(String email) {
