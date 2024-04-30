d3c2367bc721cd11c00af3d4f8ff227f3d8c0ebf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-04-30 14:47:43 +0900

    feat: 에러 코드 관리 개선 및 추가

--- src/main/java/com/thinktank/global/error/model/ErrorCode.java
+++ src/main/java/com/thinktank/global/error/model/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum ErrorCode {
-	// 400
+
+	// 400: BAD REQUEST
 	BAD_REQUEST("[❎ ERROR]"),
 	FAIL_WRONG_PASSWORD("[❎ ERROR] 비밀번호가 일치하지 않습니다."),
 	FAIL_INVALID_CATEGORY("[❎ ERROR] 유효하지 않은 카테고리입니다."),
@@ -18,21 +19,25 @@ public enum ErrorCode {
 	BAD_REQUEST_FAIL("[❎실패] 테스트케이스를 통과하지 못했습니다."),
 	BAD_REQUEST_TIME_OUT("[❎실패] 시간 초과입니다."),
 
-	// 401
+	// 401: UNAUTHORIZED
 	FAIL_UNAUTHORIZED_EXCEPTION("[❎ ERROR] 로그인이 필요한 기능입니다."),
+	FAIL_TOKEN_EXPIRED_EXCEPTION("[❎ ERROR] 인증 토큰이 만료되었습니다."),
+	FAIL_INVALID_TOKEN_EXCEPTION("[❎ ERROR] 유효하지 않은 인증 토큰입니다."),
 
-	//403
+	// 403: FORBIDDEN
 	POST_POST_FORBIDDEN_EXCEPTION("[❎ ERROR] 게시글 작성 권한이 없습니다."),
 	DELETE_POST_FORBIDDEN_EXCEPTION("[❎ ERROR] 게시글 삭제 권한이 없습니다."),
 	POST_COMMENT_FORBIDDEN_EXCEPTION("[❎ ERROR] 댓글 작성 권한이 없습니다."),
 	DELETE_COMMENT_FORBIDDEN_EXCEPTION("[❎ ERROR] 댓글 삭제 권한이 없습니다."),
 
-	// 404
+	// 404: NOT FOUNT
 	FAIL_NOT_POST_FOUND_EXCEPTION("[❎ ERROR] 요청하신 게시물을 찾을 수 없습니다."),
 	FAIL_NOT_USER_FOUND_EXCEPTION("[❎ ERROR] 요청하신 회원을 찾을 수 없습니다."),
 	FAIL_NOT_COMMENT_FOUND_EXCEPTION("[❎ ERROR] 요청하신 댓글을 찾을 수 없습니다."),
+	FAIL_NOT_TOKEN_FOUND_EXCEPTION("[❎ ERROR] 요청하신 토큰을 찾을 수 없습니다."),
+	FAIL_NOT_COOKIE_FOUND_EXCEPTION("[❎ ERROR] 요청하신 쿠키를 찾을 수 없습니다."),
 
-	// 409
+	// 409: CONFLICT
 	FAIL_EMAIL_CONFLICT("[❎ ERROR] 이미 존재하는 이메일입니다."),
 	FAIL_NICKNAME_CONFLICT("[❎ ERROR] 이미 존재하는 닉네임입니다.");
 
