b2fd217d5e151c4141c0f14762a42d172c690d14
ChangMinPark2 <98qkrckdals@naver.com>
2024-05-02 00:43:02 +0900

    feat: 공통 메서드 추가

--- src/main/java/com/thinktank/global/common/util/JudgeUtil.java
+++ src/main/java/com/thinktank/global/common/util/JudgeUtil.java
@@ -1,9 +1,12 @@
 package com.thinktank.global.common.util;
 
+import java.io.File;
 import java.util.List;
 
 import com.thinktank.api.dto.testcase.custom.TestCaseDto;
 
 public interface JudgeUtil {
 	void executeCode(List<TestCaseDto> testCases, String code);
+
+	ProcessBuilder startDockerRun(File tempDir);
 }
