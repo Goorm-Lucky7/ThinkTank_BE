8eb1134207817bcf9b5ddcbe65b64feef661e37c
ChangMinPark2 <98qkrckdals@naver.com>
2024-05-02 01:06:58 +0900

    feat: 레포지토리 메서드 추가

--- src/main/java/com/thinktank/api/repository/TestCaseRepository.java
+++ src/main/java/com/thinktank/api/repository/TestCaseRepository.java
@@ -1,8 +1,12 @@
 package com.thinktank.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.thinktank.api.entity.Post;
 import com.thinktank.api.entity.TestCase;
 
 public interface TestCaseRepository extends JpaRepository<TestCase, Long> {
+	List<TestCase> findByPost(Post post);
 }
