0c88122c98be26f53049a4e84761bf144869dc57
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-02 00:58:55 +0900

    refactor: 로그인 및 로그아웃 서비스 로직 분리

--- src/main/java/com/thinktank/api/service/UserService.java
+++ src/main/java/com/thinktank/api/service/UserService.java
@@ -1,32 +1,20 @@
 package com.thinktank.api.service;
 
-import static com.thinktank.global.common.util.AuthConstants.*;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.thinktank.api.dto.user.request.LoginReqDto;
 import com.thinktank.api.dto.user.request.SignUpDto;
-import com.thinktank.api.dto.user.response.LoginResDto;
 import com.thinktank.api.dto.user.response.UserResDto;
 import com.thinktank.api.entity.User;
 import com.thinktank.api.entity.auth.AuthUser;
 import com.thinktank.api.repository.UserRepository;
-import com.thinktank.api.service.auth.JwtProviderService;
-import com.thinktank.global.common.util.CookieUtils;
 import com.thinktank.global.error.exception.BadRequestException;
 import com.thinktank.global.error.exception.NotFoundException;
-import com.thinktank.global.error.exception.UnauthorizedException;
 import com.thinktank.global.error.model.ErrorCode;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -34,7 +22,6 @@ public class UserService {
 
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
-	private final JwtProviderService jwtProviderService;
 
 	@Transactional
 	public void signUp(SignUpDto signUpDto) {
@@ -46,38 +33,6 @@ public class UserService {
 		final User user = User.signup(signUpDto, encodedPassword);
 
 		userRepository.save(user);
-	}
-
-	@Transactional
-	public LoginResDto login(LoginReqDto loginReqDto, HttpServletResponse response) {
-		final User user = findByUserEmail(loginReqDto.email());
-
-		validatePasswordMatch(loginReqDto.password(), user.getPassword());
-
-		final String accessToken = jwtProviderService.generateAccessToken(user.getEmail(), user.getNickname());
-		final String refreshToken = jwtProviderService.generateRefreshToken(user.getEmail());
-		user.updateRefreshToken(refreshToken);
-
-		response.setHeader(ACCESS_TOKEN_HEADER, accessToken);
-
-		Cookie refreshTokenCookie = CookieUtils.generateRefreshTokenCookie(REFRESH_TOKEN_COOKIE_NAME, refreshToken);
-		response.addCookie(refreshTokenCookie);
-
-		return new LoginResDto(accessToken, refreshToken);
-	}
-
-	@Transactional
-	public void logout(HttpServletRequest request, HttpServletResponse response) {
-		String refreshToken = jwtProviderService.extractRefreshToken(REFRESH_TOKEN_COOKIE_NAME, request);
-
-		final AuthUser authUser = jwtProviderService.extractAuthUserByAccessToken(refreshToken);
-		validateRefreshToken(authUser);
-
-		final User user = findByUserEmail(authUser.email());
-		user.updateRefreshToken(null);
-
-		Cookie refreshTokenCookie = CookieUtils.expireRefreshTokenCookie(REFRESH_TOKEN_COOKIE_NAME);
-		response.addCookie(refreshTokenCookie);
 	}
 
 	public UserResDto findUserDetails(AuthUser authUser) {
@@ -95,12 +50,6 @@ public class UserService {
 			.orElseThrow(() -> new NotFoundException(ErrorCode.FAIL_NOT_USER_FOUND_EXCEPTION));
 	}
 
-	private void validateRefreshToken(AuthUser authUser) {
-		if (authUser == null) {
-			throw new UnauthorizedException(ErrorCode.FAIL_INVALID_TOKEN_EXCEPTION);
-		}
-	}
-
 	private void validateEmailNotExists(String email) {
 		if (userRepository.existsByEmail(email)) {
 			throw new BadRequestException(ErrorCode.FAIL_EMAIL_CONFLICT);
@@ -115,12 +64,6 @@ public class UserService {
 
 	private void validatePasswordEquality(String password, String checkPassword) {
 		if (!password.equals(checkPassword)) {
-			throw new BadRequestException(ErrorCode.FAIL_WRONG_PASSWORD);
-		}
-	}
-
-	private void validatePasswordMatch(String password, String encodedPassword) {
-		if (!passwordEncoder.matches(password, encodedPassword)) {
 			throw new BadRequestException(ErrorCode.FAIL_WRONG_PASSWORD);
 		}
 	}
