d1582d84f168127035561a83a4e8560e408de021
Yeeun Shim <ner.hesed@gmail.com>
2024-05-02 21:36:31 +0900

    feat: 댓글 생성 기능에 로그인 관련 기능 추가

--- src/main/java/com/thinktank/api/controller/CommentController.java
+++ src/main/java/com/thinktank/api/controller/CommentController.java
@@ -2,21 +2,21 @@ package com.thinktank.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thinktank.api.dto.comment.request.CommentCreateDto;
-import com.thinktank.api.dto.comment.response.CommentsResponse;
+import com.thinktank.api.dto.comment.request.CommentDeleteDto;
+import com.thinktank.api.dto.comment.response.CommentsResponseDto;
 import com.thinktank.api.service.CommentService;
-import com.thinktank.api.service.auth.JwtProviderService;
 
-import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -26,18 +26,26 @@ public class CommentController {
 	private final CommentService commentService;
 
 	@PostMapping("/comments")
-	public ResponseEntity<String> createComment(@RequestHeader("Authorization") String authToken,
-												@RequestBody @Validated CommentCreateDto commentCreateDto) {
-		commentService.createComment(commentCreateDto);
+	public ResponseEntity<String> createComment(@RequestBody @Validated CommentCreateDto commentCreateDto,
+												HttpServletRequest request) {
+		commentService.createComment(commentCreateDto, request);
 
 		return ResponseEntity.ok("Comment Created Successfully");
 	}
 
-	@GetMapping("/api/post/{post-id}/comments")
-	public ResponseEntity<CommentsResponse> getCommentsByPostId(@RequestHeader("Authorization") String authToken,
+	@GetMapping("/posts/{post-id}/comments")
+	public ResponseEntity<CommentsResponseDto> getCommentsByPostId(
 												@PathVariable("post-id") Long postId,
-												@RequestParam int pageIndex, @RequestParam int pageSize) {
-		CommentsResponse comments = commentService.getCommentsByPostId(postId, pageIndex, pageSize);
+												@RequestParam(required = true) Integer pageIndex,
+												@RequestParam(required = true) Integer pageSize) {
+		CommentsResponseDto comments = commentService.getCommentsByPostId(postId, pageIndex, pageSize);
 		return ResponseEntity.ok(comments);
+	}
+
+	@DeleteMapping("/comments")
+	public ResponseEntity<String> deleteComment(@RequestBody @Validated CommentDeleteDto commentDeleteDto,
+		 										HttpServletRequest request) {
+		commentService.deleteComment(commentDeleteDto, request);
+		return ResponseEntity.ok("Comment deleted successfully.");
 	}
 }
--- src/main/java/com/thinktank/api/service/CommentService.java
+++ src/main/java/com/thinktank/api/service/CommentService.java
@@ -1,21 +1,22 @@
 package com.thinktank.api.service;
 
+import static com.thinktank.global.common.util.AuthConstants.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.thinktank.api.dto.comment.request.CommentCreateDto;
-import com.thinktank.api.dto.comment.request.CommentPageRequestDto;
+import com.thinktank.api.dto.comment.request.CommentDeleteDto;
+import com.thinktank.api.dto.comment.response.CommentPageResponseDto;
 import com.thinktank.api.dto.comment.response.CommentResponseDto;
 import com.thinktank.api.dto.comment.response.CommentUserResponseDto;
-import com.thinktank.api.dto.comment.response.CommentsResponse;
+import com.thinktank.api.dto.comment.response.CommentsResponseDto;
 import com.thinktank.api.entity.Comment;
 import com.thinktank.api.entity.Post;
 import com.thinktank.api.entity.User;
@@ -23,42 +24,45 @@ import com.thinktank.api.entity.auth.AuthUser;
 import com.thinktank.api.repository.CommentRepository;
 import com.thinktank.api.repository.PostRepository;
 import com.thinktank.api.repository.UserRepository;
+import com.thinktank.api.service.auth.JwtProviderService;
 import com.thinktank.global.error.exception.BadRequestException;
 import com.thinktank.global.error.model.ErrorCode;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
-	private static final int CONTENT_MAX_LENGTH = 100;
+	private static final int DEFAULT_PAGE_INDEX = 0;
+	private static final int DEFAULT_PAGE_SIZE = 10;
 
 	private final CommentRepository commentRepository;
 	private final PostRepository postRepository;
 	private final UserRepository userRepository;
+	private final JwtProviderService jwtProviderService;
 
 	@Transactional
-	public void createComment(CommentCreateDto commentCreateDto) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		AuthUser authUser = (AuthUser) authentication.getPrincipal();
+	public void createComment(CommentCreateDto commentCreateDto, HttpServletRequest request) {
+		String accessToken = jwtProviderService.extractAccessToken(ACCESS_TOKEN_HEADER, request);
+		AuthUser authUser = jwtProviderService.extractAuthUserByAccessToken(accessToken);
 
-		if (authentication == null || !authentication.isAuthenticated()) {
-			throw new BadRequestException(ErrorCode.FAIL_UNAUTHORIZED_EXCEPTION);
-		}
-
-		User user = userRepository.findByEmail(authUser.email())
+		final User user = userRepository.findByEmail(authUser.email())
 			.orElseThrow(() -> new BadRequestException(ErrorCode.FAIL_UNAUTHORIZED_EXCEPTION));
 
 		final Post post = postRepository.findById(commentCreateDto.postId())
 				.orElseThrow(() -> new BadRequestException(ErrorCode.BAD_REQUEST));
 
-		Comment comment = Comment.create(commentCreateDto, post);
+		Comment comment = Comment.create(commentCreateDto, user, post);
 		commentRepository.save(comment);
 	}
 
 	@Transactional(readOnly = true)
-	public CommentsResponse getCommentsByPostId(Long postId, int pageIndex, int pageSize) {
-		Page<Comment> page = commentRepository.findByPostId(postId, PageRequest.of(pageIndex, pageSize));
+	public CommentsResponseDto getCommentsByPostId(Long postId, Integer pageIndex, Integer pageSize) {
+		Pageable pageable = PageRequest.of(pageIndex == null ? DEFAULT_PAGE_INDEX : pageIndex,
+											pageSize == null ? DEFAULT_PAGE_SIZE : pageSize);
+		Page<Comment> page = commentRepository.findByPostId(postId, pageable);
+
 		List<CommentResponseDto> comments = page.getContent().stream()
 			.map(comment -> new CommentResponseDto(
 				comment.getId(),
@@ -68,8 +72,38 @@ public class CommentService {
 			))
 			.collect(Collectors.toList());
 
-		CommentPageRequestDto pageRequestDto = new CommentPageRequestDto(pageIndex, !page.hasNext());
+		CommentPageResponseDto pageRequestDto = new CommentPageResponseDto(pageIndex, !page.hasNext());
 
-		return new CommentsResponse(postId, comments, pageRequestDto);
+		return new CommentsResponseDto(postId, comments, pageRequestDto);
+	}
+
+	@Transactional
+	public void deleteComment(CommentDeleteDto commentDeleteDto, HttpServletRequest request) {
+		String accessToken = jwtProviderService.extractAccessToken(ACCESS_TOKEN_HEADER, request);
+		AuthUser authUser = jwtProviderService.extractAuthUserByAccessToken(accessToken);
+
+		Comment comment = commentRepository.findById(commentDeleteDto.commentId())
+			.orElseThrow(() -> new BadRequestException(ErrorCode.FAIL_NOT_COMMENT_FOUND_EXCEPTION));
+
+		boolean isUserComment = validateUserComment(comment, authUser.email());
+		boolean isCommentInUserPost = validateCommentInUserPost(commentDeleteDto.postId(), authUser.email());
+
+		if(!isUserComment && !isCommentInUserPost) {
+			throw new BadRequestException(ErrorCode.DELETE_COMMENT_FORBIDDEN_EXCEPTION);
+		}
+
+		commentRepository.delete(comment);
+	}
+
+	private boolean validateUserComment(Comment comment, String userEmail) {
+		return comment.getUser().getEmail().equals(userEmail);
+	}
+
+	private boolean validateCommentInUserPost(Long postId, String userEmail) {
+		Post post = postRepository.findById(postId)
+			.orElseThrow(() -> new BadRequestException(ErrorCode.FAIL_NOT_POST_FOUND_EXCEPTION));
+
+		// return post.getUser().getEmail.equals(userEmail);
+		return true;
 	}
 }
