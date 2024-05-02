0b2009f5733806d98b6ea3f570b3a4d90d8c2ad3
leedidoo <665479@naver.com>
2024-05-03 02:02:39 +0900

    style: LikeController Line Seperator CR에서 LF로 변경

--- src/main/java/com/thinktank/api/service/UserLikeService.java
+++ src/main/java/com/thinktank/api/service/UserLikeService.java
@@ -1,1 +1,21 @@
-package com.thinktank.api.service;import org.springframework.stereotype.Service;import org.springframework.transaction.annotation.Transactional;import com.thinktank.api.entity.UserLike;import com.thinktank.api.repository.UserLikeRepository;import lombok.RequiredArgsConstructor;@Service@RequiredArgsConstructor@Transactional(readOnly = true)public class UserLikeService {	private final UserLikeRepository userLikeRepository;	public boolean isPostLikedByUser(Long userId, Long postId) {		UserLike userLike = userLikeRepository.findByUserIdAndLikePostId(userId, postId);		return userLike != null && userLike.isCheck();	}}
+package com.thinktank.api.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.thinktank.api.entity.UserLike;
+import com.thinktank.api.repository.UserLikeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserLikeService {
+	private final UserLikeRepository userLikeRepository;
+
+	public boolean isPostLikedByUser(Long userId, Long postId) {
+		UserLike userLike = userLikeRepository.findByUserIdAndLikePostId(userId, postId);
+		return userLike != null && userLike.isCheck();
+	}
+}
