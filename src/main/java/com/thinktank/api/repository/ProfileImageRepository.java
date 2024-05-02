103c6da809e4707512509bcdd140175fb7e3d1fd
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-02 18:01:39 +0900

    feat: 사용자 이메일로 저장된 이미지를 조회하는 로직 구현

--- src/main/java/com/thinktank/api/repository/ProfileImageRepository.java
+++ src/main/java/com/thinktank/api/repository/ProfileImageRepository.java
@@ -1,8 +1,12 @@
 package com.thinktank.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.thinktank.api.entity.ProfileImage;
 
 public interface ProfileImageRepository extends JpaRepository<ProfileImage, Long> {
+
+	Optional<ProfileImage> findByUserEmail(String email);
 }
