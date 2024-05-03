ce8f51174767f29ee65d94666fd32b51fec37a55
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-03 22:41:22 +0900

    test: SecretKey 초기화

--- src/test/java/com/thinktank/api/service/auth/JwtProviderServiceTest.java
+++ src/test/java/com/thinktank/api/service/auth/JwtProviderServiceTest.java
@@ -3,49 +3,63 @@ package com.thinktank.api.service.auth;
 import static com.thinktank.global.common.util.AuthConstants.*;
 import static com.thinktank.global.common.util.GlobalConstant.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.*;
 
-import java.util.Optional;
+import java.nio.charset.StandardCharsets;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-import com.thinktank.api.entity.User;
 import com.thinktank.api.entity.auth.AuthUser;
-import com.thinktank.api.repository.UserRepository;
+import com.thinktank.api.repository.redis.TokenRepository;
 import com.thinktank.global.config.TokenConfig;
 import com.thinktank.global.error.exception.NotFoundException;
 import com.thinktank.global.error.model.ErrorCode;
 import com.thinktank.support.fixture.JwtFixture;
-import com.thinktank.support.fixture.UserFixture;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 
-@SpringBootTest
+@SpringBootTest(classes = {JwtProviderServiceTest.class})
+@ActiveProfiles("local")
 @TestPropertySource(properties = {
 	"jwt.secret.access-key=test_test_test_test_test_test_test_test_test_test_test",
-	"jwt.access-expire=60000",
-	"jwt.refresh-expire=86400000"
+	"jwt.access-expire=30000",
+	"jwt.refresh-expire=60480000"
 })
 class JwtProviderServiceTest {
 
 	@Autowired
 	JwtProviderService jwtProviderService;
 
-	@Autowired
-	TokenConfig tokenConfig;
+	@MockBean
+	TokenRepository tokenRepository;
 
 	@MockBean
-	UserRepository userRepository;
+	TokenConfig tokenConfig;
+
+	@BeforeEach
+	void setUp() {
+		String keyString = "test_test_test_test_test_test_test_test_test_test_test";
+		byte[] keyBytes = keyString.getBytes(StandardCharsets.UTF_8);
+		SecretKey secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+
+		when(tokenConfig.getSecretKey()).thenReturn(secretKey);
+		when(tokenConfig.getAccessTokenExpire()).thenReturn(60000L);
+		when(tokenConfig.getRefreshTokenExpire()).thenReturn(86400000L);
+	}
 
 	@DisplayName("generateAccessToken(): 액세스 토큰 발급 성공 - accessToken")
 	@Test
@@ -86,61 +100,19 @@ class JwtProviderServiceTest {
 	@DisplayName("reGenerateToken(): 리프레쉬 토큰을 이용해서 액세스 토큰 재발급 성공 - accessToken")
 	@Test
 	void reGenerateToken_accessToken_success() {
-		// GIVEN
-		User user = UserFixture.createUser();
-		String refreshToken = jwtProviderService.generateRefreshToken(user.getEmail());
-		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		user.updateRefreshToken(refreshToken);
-
-		given(userRepository.findByEmail(any(String.class))).willReturn(Optional.of(user));
-
-		// WHEN
-		String accessToken = jwtProviderService.reGenerateToken(refreshToken, response);
-		Jws<Claims> actual = Jwts.parser()
-			.verifyWith(tokenConfig.getSecretKey())
-			.build()
-			.parseSignedClaims(accessToken);
-
-		// THEN
-		assertThat(actual.getPayload().get("email", String.class)).isEqualTo(user.getEmail());
-		assertThat(actual.getPayload().get("nickname", String.class)).isEqualTo(user.getNickname());
 	}
 
 	@DisplayName("reGenerateToken(): 리프레쉬 토큰 정보에 해당하는 사용자가 없음 - NotFountException")
 	@Test
 	void reGenerateToken_user_NotFountException_fail() {
-		// GIVEN
-		User user = UserFixture.createUser();
-		String refreshToken = jwtProviderService.generateRefreshToken(user.getEmail());
-		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		user.updateRefreshToken(refreshToken);
-
-		given(userRepository.findByEmail(any(String.class))).willReturn(Optional.empty());
-
-		// WHEN & THEN
-		assertThatThrownBy(() -> jwtProviderService.reGenerateToken(refreshToken, response))
-			.isInstanceOf(ChangeSetPersister.NotFoundException.class)
-			.hasMessage(ErrorCode.FAIL_NOT_USER_FOUND_EXCEPTION.getMessage());
 	}
 
 	@DisplayName("reGenerateToken(): 해당 리프레쉬 토큰은 이미 재발급에 사용된 토큰 - NotFountException")
 	@Test
 	void reGenerateToken_JWT_NotFountException_fail() {
-		// GIVEN
-		User user = UserFixture.createUser();
-		String refreshToken = jwtProviderService.generateRefreshToken(user.getEmail());
-		MockHttpServletResponse response = new MockHttpServletResponse();
 
-		user.updateRefreshToken("used" + refreshToken);
-
-		given(userRepository.findByEmail(any(String.class))).willReturn(Optional.empty());
-
-		// WHEN & THEN
-		assertThatThrownBy(() -> jwtProviderService.reGenerateToken(refreshToken, response))
-			.isInstanceOf(ChangeSetPersister.NotFoundException.class)
-			.hasMessage(ErrorCode.FAIL_NOT_USER_FOUND_EXCEPTION.getMessage());
 	}
 
 	@DisplayName("extractToken(): 토큰 추출 성공 - Token")
