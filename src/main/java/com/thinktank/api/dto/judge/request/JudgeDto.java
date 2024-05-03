1082a9ca730504a5b9e478ac4f2845b55125b07d
ChangMinPark2 <98qkrckdals@naver.com>
2024-05-03 18:35:53 +0900

    refactor: dto 수정

--- src/main/java/com/thinktank/api/dto/judge/request/JudgeDto.java
+++ src/main/java/com/thinktank/api/dto/judge/request/JudgeDto.java
@@ -1,9 +1,5 @@
 package com.thinktank.api.dto.judge.request;
 
-import java.util.List;
-
-import com.thinktank.api.dto.testcase.custom.CustomTestCase;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -12,8 +8,6 @@ public record JudgeDto(
 	Long postId,
 	@NotBlank(message = "[❎ ERROR] 언어는 필수 입력값입니다")
 	String language,
-
-	List<CustomTestCase> testCases,
 	@NotBlank(message = "[❎ ERROR] 정답 코드는 필수 입력값입니다.")
 	String code
 ) {
