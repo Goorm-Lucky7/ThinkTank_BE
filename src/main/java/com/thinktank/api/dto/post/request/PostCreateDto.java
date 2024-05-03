994eeeccd71ae6c70b712e3caf82a8e591d2d19b
leedidoo <665479@naver.com>
2024-05-03 15:32:21 +0900

    feat: 게시글 생성시 테스트 케이스 추가

--- src/main/java/com/thinktank/api/dto/post/request/PostCreateDto.java
+++ src/main/java/com/thinktank/api/dto/post/request/PostCreateDto.java
@@ -1,8 +1,13 @@
 package com.thinktank.api.dto.post.request;
 
+import java.util.List;
+
 import org.hibernate.validator.constraints.Length;
 
+import com.thinktank.api.dto.testcase.custom.CustomTestCase;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record PostCreateDto(
 	@NotBlank @Length(max = 20, message = "[❎ ERROR] 제목은 최대 20자 입니다.")
@@ -19,6 +24,8 @@ public record PostCreateDto(
 
 	@NotBlank(message = "[❎ ERROR] 제약 조건은 필수 입력값입니다.")
 	String condition,
+	@NotNull(message = "[❎ ERROR] 테스트케이스는 필수 입력값입니다.")
+	List<CustomTestCase> testCases,
 
 	@NotBlank(message = "[❎ ERROR] 정답 코드는 필수 입력값입니다.")
 	String answer
--- src/main/java/com/thinktank/api/service/PostService.java
+++ src/main/java/com/thinktank/api/service/PostService.java
@@ -2,6 +2,7 @@ package com.thinktank.api.service;
 
 import static com.thinktank.global.common.util.GlobalConstant.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,15 +16,18 @@ import com.thinktank.api.dto.page.response.PageInfoDto;
 import com.thinktank.api.dto.post.request.PostCreateDto;
 import com.thinktank.api.dto.post.response.PagePostResponseDto;
 import com.thinktank.api.dto.post.response.PostsResponseDto;
+import com.thinktank.api.dto.testcase.custom.CustomTestCase;
 import com.thinktank.api.dto.user.response.SimpleUserResDto;
 import com.thinktank.api.entity.Category;
 import com.thinktank.api.entity.Language;
 import com.thinktank.api.entity.Post;
+import com.thinktank.api.entity.TestCase;
 import com.thinktank.api.entity.User;
 import com.thinktank.api.entity.auth.AuthUser;
 import com.thinktank.api.repository.CommentRepository;
 import com.thinktank.api.repository.LikeRepository;
 import com.thinktank.api.repository.PostRepository;
+import com.thinktank.api.repository.TestCaseRepository;
 import com.thinktank.api.repository.UserRepository;
 import com.thinktank.global.error.exception.BadRequestException;
 import com.thinktank.global.error.model.ErrorCode;
@@ -39,6 +43,7 @@ public class PostService {
 	private final UserRepository userRepository;
 	private final LikeRepository likeRepository;
 	private final CommentRepository commentRepository;
+	private final TestCaseRepository testCaseRepository;
 	private final UserLikeService userLikeService;
 
 	public void createPost(PostCreateDto postCreateDto, AuthUser authUser) {
@@ -48,6 +53,13 @@ public class PostService {
 		validateLanguage(postCreateDto.language());
 		Post post = Post.create(postCreateDto, user);
 		postRepository.save(post);
+		List<CustomTestCase> testCases = postCreateDto.testCases();
+		List<TestCase> savedTestCases = new ArrayList<>();
+		for (CustomTestCase customTestCase : testCases) {
+			TestCase testCase = TestCase.createTestCase(customTestCase, post);
+			savedTestCases.add(testCase);
+		}
+		testCaseRepository.saveAll(savedTestCases);
 	}
 
 	public PagePostResponseDto getAllPosts(int page, int size, AuthUser authUser) {
