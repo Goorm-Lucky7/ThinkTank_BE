4c154cecc9554cbb36587d870144ce26917d8a65
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-07 15:36:33 +0900

    refactor: 액세스 토큰 만료 시 자동 재발급되도록 구현

--- src/main/java/com/thinktank/global/auth/filter/AuthenticationFilter.java
+++ src/main/java/com/thinktank/global/auth/filter/AuthenticationFilter.java
@@ -3,6 +3,8 @@ package com.thinktank.global.auth.filter;
 import static com.thinktank.global.common.util.AuthConstants.*;
 import static com.thinktank.global.common.util.GlobalConstant.*;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import com.thinktank.api.dto.auth.TokenResDto;
 import com.thinktank.api.entity.auth.AuthUser;
 import com.thinktank.api.service.auth.JwtProviderService;
 import com.thinktank.global.auth.AuthorizationThreadLocal;
@@ -17,6 +20,7 @@ import com.thinktank.global.error.exception.NotFoundException;
 import com.thinktank.global.error.model.ErrorCode;
 
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
@@ -40,29 +44,27 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(
 		@NotNull HttpServletRequest request,
 		@NotNull HttpServletResponse response,
-		@NotNull FilterChain filterChain
-	) {
-		String accessToken = jwtProviderService.extractAccessToken(ACCESS_TOKEN_HEADER, request);
-		String refreshToken = jwtProviderService.extractRefreshToken(REFRESH_TOKEN_COOKIE_NAME, request);
-
+		@NotNull FilterChain filterChain) throws ServletException, IOException {
 		try {
-			if (jwtProviderService.isUsable(accessToken, response)) {
+			String accessToken = jwtProviderService.extractAccessToken(ACCESS_TOKEN_HEADER, request);
+
+			if (jwtProviderService.isUsable(accessToken)) {
 				setAuthentication(accessToken);
 				filterChain.doFilter(request, response);
-				return;
-			}
+			} else {
+				String refreshToken = jwtProviderService.extractRefreshToken(REFRESH_TOKEN_COOKIE_NAME, request);
 
-			if (jwtProviderService.isUsable(refreshToken, response)) {
-				accessToken = jwtProviderService.reGenerateToken(refreshToken, response);
-				setAuthentication(accessToken);
-				filterChain.doFilter(request, response);
-				return;
+				if (jwtProviderService.isUsable(refreshToken)) {
+					TokenResDto tokenResDto = jwtProviderService.reGenerateToken(refreshToken, response);
+					accessToken = tokenResDto.accessToken();
+					setAuthentication(accessToken);
+					filterChain.doFilter(request, response);
+				} else {
+					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					handlerExceptionResolver.resolveException(request, response, null,
+						new NotFoundException(ErrorCode.FAIL_TOKEN_EXPIRED_EXCEPTION));
+				}
 			}
-
-			throw new NotFoundException(ErrorCode.FAIL_TOKEN_EXPIRED_EXCEPTION);
-		} catch (Exception e) {
-			log.warn("JWT ERROR DESCRIPTION");
-			handlerExceptionResolver.resolveException(request, response, null, e);
 		} finally {
 			AuthorizationThreadLocal.remove();
 		}
