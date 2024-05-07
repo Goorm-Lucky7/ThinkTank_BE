d7fbac51c7faf5fcdcbb34780b70c45f712f83d9
leedidoo <665479@naver.com>
2024-05-07 12:43:27 +0900

    refactor: 게시글 조회 시 실제 정답자 수 받아올 수 있도록 수정,게시글 삭제 시 관련 유저 코드도 함께 삭제되도록 수정

--- src/main/java/com/thinktank/api/repository/UserCodeRepository.java
+++ src/main/java/com/thinktank/api/repository/UserCodeRepository.java
@@ -1,6 +1,8 @@
 package com.thinktank.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.thinktank.api.entity.Post;
 import com.thinktank.api.entity.User;
@@ -8,4 +10,9 @@ import com.thinktank.api.entity.UserCode;
 
 public interface UserCodeRepository extends JpaRepository<UserCode, Long> {
 	Boolean existsByUserAndPost(User user, Post post);
+
+	@Query("SELECT COUNT(uc) FROM UserCode uc WHERE uc.post = :post")
+	int countUserCodeByPost(@Param("post") Post post);
+
+	void deleteByPostId(Long postId);
 }
