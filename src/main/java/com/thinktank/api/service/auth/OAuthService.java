bcb58a94ae2acc8884555d2d9eb0bf8740324fd2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-11 21:42:35 +0900

    refactor: 카카오 및 구글 로그인 로직 구현

--- src/main/java/com/thinktank/api/service/auth/OAuthService.java
+++ src/main/java/com/thinktank/api/service/auth/OAuthService.java
@@ -1,11 +1,15 @@
 package com.thinktank.api.service.auth;
 
+import java.io.IOException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.thinktank.api.dto.auth.KakaoOAuthTokenDto;
 import com.thinktank.api.dto.auth.KakaoUserInfoResDto;
+import com.thinktank.api.dto.auth.GoogleOAuthTokenDto;
+import com.thinktank.api.dto.auth.GoogleUserInfoResDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,9 +18,14 @@ import lombok.RequiredArgsConstructor;
 public class OAuthService {
 
 	private final KakaoAuthService kakaoAuthService;
+	private final GoogleAuthService googleAuthService;
 
 	public KakaoUserInfoResDto kakaoLogin(String code) throws JsonProcessingException {
 		return getKakaoUserInfoDto(code);
+	}
+
+	public GoogleUserInfoResDto googleLogin(String code) throws IOException {
+		return getGoogleUserInfoDto(code);
 	}
 
 	private KakaoUserInfoResDto getKakaoUserInfoDto(String code) throws JsonProcessingException {
@@ -26,5 +35,14 @@ public class OAuthService {
 		ResponseEntity<String> userInfoResponse = kakaoAuthService.requestUserInfo(kakaoOAuthTokenDto);
 
 		return kakaoAuthService.getUserInfo(userInfoResponse);
+	}
+
+	private GoogleUserInfoResDto getGoogleUserInfoDto(String code) throws JsonProcessingException {
+		ResponseEntity<String> accessToken = googleAuthService.requestAccessToken(code);
+		GoogleOAuthTokenDto googleOAuthTokenDto = googleAuthService.getAccessToken(accessToken);
+
+		ResponseEntity<String> userInfoResponse = googleAuthService.requestUserInfo(googleOAuthTokenDto);
+
+		return googleAuthService.getUserInfo(userInfoResponse);
 	}
 }
