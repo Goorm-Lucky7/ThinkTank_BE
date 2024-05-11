0aab62eee465be4cb627f71fb6da885a9d288684
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-11 17:19:55 +0900

    feat: 카카오 로그인 로직 구현

--- src/main/java/com/thinktank/api/service/auth/AuthenticationService.java
+++ src/main/java/com/thinktank/api/service/auth/AuthenticationService.java
@@ -6,12 +6,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.thinktank.api.dto.TokenReqDto;
-import com.thinktank.api.dto.TokenResDto;
+import com.thinktank.api.dto.auth.KakaoLoginReqDto;
 import com.thinktank.api.dto.user.request.LoginReqDto;
 import com.thinktank.api.dto.user.response.LoginResDto;
 import com.thinktank.api.entity.User;
 import com.thinktank.api.repository.UserRepository;
+import com.thinktank.api.service.UserProfileService;
 import com.thinktank.global.error.exception.BadRequestException;
 import com.thinktank.global.error.exception.NotFoundException;
 import com.thinktank.global.error.model.ErrorCode;
@@ -28,25 +28,37 @@ public class AuthenticationService {
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 	private final JwtProviderService jwtProviderService;
+	private final UserProfileService userProfileService;
 
 	@Transactional
 	public LoginResDto login(LoginReqDto loginReqDto, HttpServletResponse response) {
 		final User user = findByUserEmail(loginReqDto.email());
 		validatePasswordMatch(loginReqDto.password(), user.getPassword());
 
-		final String accessToken = jwtProviderService.generateToken(user.getEmail(), user.getNickname());
+		final String accessToken = jwtProviderService.generateAccessToken(user.getEmail(), user.getNickname());
+		final String refreshToken = jwtProviderService.generateRefreshToken(user.getEmail());
+		user.updateRefreshToken(refreshToken);
 
 		response.setHeader(ACCESS_TOKEN_HEADER, accessToken);
+		response.setHeader(REFRESH_TOKEN_HEADER, refreshToken);
 
-		return new LoginResDto(accessToken);
+		return new LoginResDto(accessToken, refreshToken);
 	}
 
-	public TokenResDto reissue(TokenReqDto tokenReqDto, HttpServletResponse response) {
-		final String newAccessToken = jwtProviderService.reGenerateToken(tokenReqDto.expiredToken());
+	@Transactional
+	public LoginResDto kakaoLogin(KakaoLoginReqDto kakaoLoginReqDto, HttpServletResponse response) {
+		final User user = userRepository.findByEmail(kakaoLoginReqDto.email())
+			.orElseGet(() -> registerUser(kakaoLoginReqDto));
+		userProfileService.createProfileImage(user);
 
-		response.setHeader(ACCESS_TOKEN_HEADER, newAccessToken);
+		final String accessToken = jwtProviderService.generateAccessToken(user.getEmail(), user.getNickname());
+		final String refreshToken = jwtProviderService.generateRefreshToken(user.getRefreshToken());
+		user.updateRefreshToken(refreshToken);
 
-		return new TokenResDto(newAccessToken);
+		response.setHeader(ACCESS_TOKEN_HEADER, accessToken);
+		response.setHeader(REFRESH_TOKEN_HEADER, refreshToken);
+
+		return new LoginResDto(accessToken, refreshToken);
 	}
 
 	private User findByUserEmail(String email) {
@@ -58,5 +70,13 @@ public class AuthenticationService {
 		if (!passwordEncoder.matches(password, encodedPassword)) {
 			throw new BadRequestException(ErrorCode.FAIL_WRONG_PASSWORD);
 		}
+	}
+
+	private User registerUser(KakaoLoginReqDto kakaoLoginReqDto) {
+		final User user = User.kakaoSignup(
+			kakaoLoginReqDto, passwordEncoder.encode("SecureRandomPassword")
+		);
+
+		return userRepository.save(user);
 	}
 }
