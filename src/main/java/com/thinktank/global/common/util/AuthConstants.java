3220b0a2bff5fc85edd0801772bf72dee5ad998d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-12 04:00:20 +0900

    feat: 리다이렉트 주소 상수화

--- src/main/java/com/thinktank/global/common/util/AuthConstants.java
+++ src/main/java/com/thinktank/global/common/util/AuthConstants.java
@@ -9,4 +9,10 @@ public final class AuthConstants {
 	public static final String ACCESS_TOKEN_HEADER = "Authorization";
 	public static final String REFRESH_TOKEN_HEADER = "refresh";
 	public static final String BEARER = "Bearer";
+
+	public static final String CLIENT_ID = "?client_id=";
+	public static final String REDIRECT_URI = "&redirect_uri=";
+	public static final String RESPONSE_TYPE = "&response_type=code";
+	public static final String SCOPE = "&scope=";
+	public static final String INFO = "email profile";
 }
