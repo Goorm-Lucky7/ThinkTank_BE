a99816b5c21284166a1b98cc4658e689fb7f9234
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-12 20:19:02 +0900

    refactor: DTO 통합

--- src/main/java/com/thinktank/api/dto/auth/OAuthTokenResDto.java
+++ src/main/java/com/thinktank/api/dto/auth/OAuthTokenResDto.java
@@ -1,12 +1,16 @@
 package com.thinktank.api.dto.auth;
 
+import java.util.Optional;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record GoogleOAuthTokenDto(
+public record OAuthTokenResDto(
 	@JsonProperty("access_token") String accessToken,
 	@JsonProperty("expires_in") String expiresIn,
 	@JsonProperty("scope") String scope,
 	@JsonProperty("token_type") String tokenType,
-	@JsonProperty("id_token") String idToken
+	@JsonProperty("id_token") Optional<String> idToken,
+	@JsonProperty("refresh_token") Optional<String> refreshToken,
+	@JsonProperty("refresh_token_expires_in") Optional<String> refreshTokenExpiresIn
 ) {
 }
