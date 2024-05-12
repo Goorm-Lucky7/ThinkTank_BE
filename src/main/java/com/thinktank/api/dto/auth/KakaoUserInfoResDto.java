2d1d206570587dd84492510c9b5f8abe956010d9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-12 20:18:44 +0900

    refactor: DTO 정리

--- src/main/java/com/thinktank/api/dto/auth/KakaoUserInfoResDto.java
+++ src/main/java/com/thinktank/api/dto/auth/KakaoUserInfoResDto.java
@@ -3,7 +3,35 @@ package com.thinktank.api.dto.auth;
 public record KakaoUserInfoResDto(
 	String id,
 	String connectedAt,
-	KakaoProperties kakaoProperties,
-	KakaoAccount kakaoAccount
+	Properties properties,
+	Account account
 ) {
+
+	public record Properties(
+		String nickname,
+		String profileImage,
+		String thumbnailImage
+	) {
+	}
+
+	public record Account(
+		String email,
+		boolean profileNicknameAgreement,
+		boolean profileImageAgreement,
+		boolean nameAgreement,
+		boolean emailAgreement,
+		boolean hasEmail,
+		boolean isEmailValid,
+		boolean isEmailVerified,
+		Profile profile
+	) {
+
+		public record Profile(
+			String nickname,
+			String thumbnailImageUrl,
+			String profileImageUrl,
+			boolean isDefaultImage
+		) {
+		}
+	}
 }
