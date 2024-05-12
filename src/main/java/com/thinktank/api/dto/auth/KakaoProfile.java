30ce442126a8dccdb839213006bbbcd1c8c5d354
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-12 20:17:48 +0900

    remove: 불필요한 DTO 제거

--- src/main/java/com/thinktank/api/dto/auth/KakaoProfile.java
+++
@@ -1,9 +0,0 @@
-package com.thinktank.api.dto.auth;
-
-public record KakaoProfile(
-	String nickname,
-	String thumbnailImageUrl,
-	String profileImageUrl,
-	boolean isDefaultImage
-) {
-}
--- src/main/java/com/thinktank/api/dto/auth/KakaoProperties.java
+++
@@ -1,8 +0,0 @@
-package com.thinktank.api.dto.auth;
-
-public record KakaoProperties(
-	String nickname,
-	String profileImage,
-	String thumbnailImage
-) {
-}
