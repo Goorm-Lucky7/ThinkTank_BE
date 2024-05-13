ecddcbc87e86bd8ea7a5f43f61f7ff152f62a255
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-13 11:26:37 +0900

    refactor: 사용자 정보 수정 로직 변경

--- src/main/java/com/thinktank/api/controller/UserController.java
+++ src/main/java/com/thinktank/api/controller/UserController.java
@@ -4,7 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,7 +17,9 @@ import com.thinktank.api.dto.TokenResDto;
 import com.thinktank.api.dto.user.request.LoginReqDto;
 import com.thinktank.api.dto.user.request.SignUpDto;
 import com.thinktank.api.dto.user.request.UserDeleteDto;
+import com.thinktank.api.dto.user.request.UserUpdateDto;
 import com.thinktank.api.dto.user.response.LoginResDto;
+import com.thinktank.api.dto.user.response.UserResDto;
 import com.thinktank.api.entity.auth.AuthUser;
 import com.thinktank.api.service.UserService;
 import com.thinktank.api.service.auth.AuthenticationService;
@@ -47,17 +51,32 @@ public class UserController {
 		return ResponseEntity.ok(authenticationService.login(loginReqDto, response));
 	}
 
+	@PostMapping("/reissue")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<TokenResDto> reissue(@RequestBody TokenReqDto tokenReqDto, HttpServletResponse response) {
+		return ResponseEntity.ok(authenticationService.reissue(tokenReqDto, response));
+	}
+
+	@GetMapping("/mypage/users")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<UserResDto> getOwnProfileDetails(@Auth AuthUser authUser) {
+		return ResponseEntity.ok(userService.getOwnProfileDetails(authUser));
+	}
+
+	@PutMapping("/mypage/users")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<String> updateUserDetails(
+		@Auth AuthUser authUser,
+		@RequestBody @Validated UserUpdateDto userUpdateDto) {
+		userService.updateUserDetails(authUser, userUpdateDto);
+		return ResponseEntity.ok("OK");
+	}
+
 	@DeleteMapping
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<String> removeUser(@Auth AuthUser authUser,
 		@RequestBody @Validated UserDeleteDto userDeleteDto) {
 		userService.removeUser(authUser, userDeleteDto);
 		return ResponseEntity.ok("OK");
-	}
-
-	@PostMapping("/reissue")
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<TokenResDto> reissue(@RequestBody TokenReqDto tokenReqDto, HttpServletResponse response) {
-		return ResponseEntity.ok(authenticationService.reissue(tokenReqDto, response));
 	}
 }
