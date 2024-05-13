eab04c96acdc75d89c2748ec1f5076063e3c35de
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-13 12:40:09 +0900

    remove: 불필요한 DTO 제거

--- src/main/java/com/thinktank/api/dto/user/request/UserDeleteDto.java
+++
@@ -1,9 +0,0 @@
-package com.thinktank.api.dto.user.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UserDeleteDto(
-	@NotBlank(message = "[❎ ERROR] 비밀번호를 입력해주세요.")
-	String password
-) {
-}
