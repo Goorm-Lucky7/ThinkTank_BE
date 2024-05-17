659dbcf364bcd3bd3667a1ecf3baaa430abb5eaf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-17 16:56:58 +0900

    refactor: 연관관계 매핑 수정

--- src/main/java/com/thinktank/api/entity/UserCode.java
+++ src/main/java/com/thinktank/api/entity/UserCode.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -27,7 +26,7 @@ public class UserCode {
 	@Column(name = "code", nullable = false, length = 1000)
 	private String code;
 
-	@OneToOne(fetch = FetchType.LAZY, orphanRemoval = true)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "post_id")
 	private Post post;
 
