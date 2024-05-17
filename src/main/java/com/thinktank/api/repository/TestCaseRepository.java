421fab76c8db359550f3d2e68988db91112aa568
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-05-17 14:23:20 +0900

    refactor: 게시글 ID를 이용한 삭제 로직을 게시글 엔티티로 변경

--- src/main/java/com/thinktank/api/repository/TestCaseRepository.java
+++ src/main/java/com/thinktank/api/repository/TestCaseRepository.java
@@ -9,9 +9,10 @@ import com.thinktank.api.entity.Post;
 import com.thinktank.api.entity.TestCase;
 
 public interface TestCaseRepository extends JpaRepository<TestCase, Long> {
+
 	List<TestCase> findByPost(Post post);
 
 	List<CustomTestCase> findByPostId(Long postId);
 
-	void deleteByPostId(Long postId);
+	void deleteByPost(Post post);
 }
